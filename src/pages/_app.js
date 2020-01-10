457f9c49707475235058b48adde27e2559b6b8f1
rjerue <ryan.jerue@viasat.com>
2020-01-09 17:02:57 -0800

    fin

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,27 +1,23 @@
-import React from 'react'
-import App from 'next/app'
-import { Tina, TinaCMS } from 'tinacms'
-import { GitClient } from '@tinacms/git-client'
+import React from "react";
+import App from "next/app";
+import { Tina, TinaCMS } from "tinacms";
+import { GitClient } from "@tinacms/git-client";
 
 class MyApp extends App {
   constructor() {
-    super()
-    this.cms = new TinaCMS()
-    const client = new GitClient('http://localhost:3000/___tina')
-    this.cms.registerApi('git', client)
+    super();
+    this.cms = new TinaCMS();
+    const client = new GitClient("http://localhost:3000/___tina");
+    this.cms.registerApi("git", client);
   }
-  options = {
-      sidebar: {
-        hidden: process.env.NODE_ENV === "production"
-      }
-  }
+
   render() {
-    const { Component, pageProps } = this.props
+    const { Component, pageProps } = this.props;
     return (
-      <Tina cms={this.cms} {...this.options.sidebar}>
+      <Tina hidden={false} cms={this.cms}>
         <Component {...pageProps} />
       </Tina>
-    )
+    );
   }
 }
-export default MyApp
+export default MyApp;
